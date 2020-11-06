4d1dd34ee760cf82449796beb0e3a42cef5041f1
Diana Mukhanova <diananova2508@gmail.com>
2020-11-06 01:39:10 +0100

    basic setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        DeepFake
       </header>
+      <body>
+        <p>Upload a face picture. (min 256x256) </p>
+        <button>Upload</button>
+
+      </body>
     </div>
   );
 }
